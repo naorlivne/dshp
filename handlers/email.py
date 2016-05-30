299b4a4be250abd6d603b06573a9b45cdaf6f2c6
naorlivne <naorlivne@gmail.com>
2016-05-30 18:35:25 +0300

    starting work on the email handler

--- handlers/email.py
+++ handlers/email.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python2.7
-import sys
-handler_json = sys.argv[1]
-file = open("example.txt", "w")
-file.write(handler_json)
-file.close()
+import sys, json, os
+json_args = json.loads(sys.argv[1])
+hostname = json_args["hostname"]
+ip = json_args["ip"]
+time = json_args["time"]
+try:
+    mail_from = os.environ["MAIL_FROM"]
+    smtp_server = os.environ["SMTP_SERVER"]
+    smtp_port = os.environ["SMTP_PORT"]
+    smtp_user = os.environ["SMTP_USER"]
+    smtp_pass = os.environ["SMTP_PASS"]
+    mail_to = os.environ["MAIL_TO"]
+except:
+    print "unable to send mail - missing one of the envvars of the email.py handler"
+    exit(2)
+print hostname, ip, time, mail_from, smtp_user
